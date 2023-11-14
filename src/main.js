3fcd31cad20eb96d830ae64f577c51b8e1d5c653
Chau MAk Ka Ra <rab2014779@student.ctu.edu.vn>
2023-11-14 14:00:54 +0700

    commit main

--- src/main.js
+++ src/main.js
@@ -4,10 +4,7 @@ import store from './store';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import router from "./router";
-<<<<<<< HEAD
 
-=======
->>>>>>> b44a6ae89f0aeecd0822b56dcf71d0f5c5643f93
 
 const app = createApp(App)
 app.use(store)
